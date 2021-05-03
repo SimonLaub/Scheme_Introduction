a3ca3c9b69dc589a551228d0f6a5a24749eda03e
Simon Laub <43849603+SimonLaub@users.noreply.github.com>
2021-05-03 20:25:56 +0200

    Add files via upload

---
+++ Intro_Scheme.scm
@@ -0,0 +1,27 @@
+; Getting started with Scheme/Racket
+; Prog Spring 21
+; 
+; Sila. April 30th 2021.
+;
+; Try it out:
+; Misc functions in Scheme
+
+(define pi (/ 22 7))
+
+(define (cubic z) (* (* z z) z))
+
+(define (superabs x)
+    (cond ((> x 0) x)
+          ((= x 0) 0)
+          ((< x 0) (* x -1))))
+
+(define (newsupabs x)
+    (if (>= 5 8) 3 (+ 7 2)))
+
+(define (newsuperabs x)
+    (if (>= x 0) x (* x -1)))
+
+(define (fact n)
+    (if (= n 0)
+        1
+        (* n (fact (- n 1)))))
